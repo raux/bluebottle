deccea6ad6cfc7db89364ad45781ac15086f3c3a
Loek van Gent <loek@onepercentclub.com>
2018-07-15 09:21:20 +0200

    sql for creating function for union views

--- sql/refresh_union_views.sql
+++ sql/refresh_union_views.sql
@@ -1,15 +1,25 @@
 CREATE OR REPLACE FUNCTION refresh_union_view(table_name text) RETURNS void AS $$
 DECLARE
   schema RECORD;
+  field RECORD;
   result RECORD;
+  fields TEXT := '';
   sql TEXT := '';
 BEGIN
+  FOR field IN EXECUTE
+    format(
+      'SELECT column_name FROM information_schema.columns WHERE table_schema = ''onepercent'' AND table_name = ''%I''',
+        table_name
+    )
+  LOOP
+    fields := fields || format('%I, ', field.column_name);
+  END LOOP;
   FOR schema IN EXECUTE
     format(
       'SELECT id, schema_name FROM public.clients_client where schema_name != ''goodup_demo'''
     )
   LOOP
-    sql := sql || format('SELECT (SELECT column_name FROM information_schema.columns WHERE table_schema = ''onepercent'' AND table_name  = ''%I''),  ''%I'' as tenant  FROM %I.%I UNION ALL ', table_name, schema.schema_name, schema.schema_name, table_name);
+    sql := sql || 'SELECT ' || fields || format('%L AS tenant  FROM %I.%I UNION ALL ', schema.schema_name, schema.schema_name, table_name);
   END LOOP;
 
   EXECUTE
